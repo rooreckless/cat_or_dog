1c76c3c7ec2b2aeffe18099c52e08c40119a7cff
足立守 <rooreckless@gmail.com>
2020-03-16 16:46:42 +0900

    capistrano導入 その2
    その1の続きとして、unicorn関連の設定を返納しています。
    また、EC2インスタンス上では、nginxも同様に
    /etc/nginx/conf.d/rails.confを変更しています。
    
    #upstream app_serverでは、server unix:の、/var/www/cat_or_dog/tmp/sockets/unicorn.sock;を、/var/www/cat_or_dog/shared/tmp/sockets/unicorn.sock;に変更
    
    #serverでは、
    root /var/www/cat_or_dog/public
    を
    root /var/www/cat_or_dog/current/public
    に変更。
    
    serverの、location ^~ /assets/　の
    root /var/www/cat_or_dog/publicを
    root /var/www/cat_or_dog/current/public

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,25 +1,31 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
+#capistrano導入に伴い変化するディレクトリ構造に対応するため、../をふやしています。
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+working_directory "#{app_path}/current"
+#capistrano導入に伴い、最新の本番環境はcat_or_dog/currentに入るため、working_directroyを変更
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+#capistrano導入に伴い、sharedディレクトリ内にある、unicorn.pidファイルを読むよう変更。
 
 #ポート番号を指定
 #listen 3000
 #ポート番号3000で待つのではなく、nginxからアクセスされた場合になるよう変更
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+#capistrano導入に伴い、sharedディレクトリ内にある、sockファイルを読むよう変更。
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+#capistrano導入に伴い、sharedディレクトリ内にあるunicorn.stderr.logを読むよう変更。
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+#capistrano導入に伴い、sharedディレクトリ内にあるunicorn.stdout.logを読むよう変更。
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
