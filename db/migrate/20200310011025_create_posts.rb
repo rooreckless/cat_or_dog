0874ab866524b4fed21126d3ba07d01354389465
足立守 <rooreckless@gmail.com>
2020-03-16 13:13:11 +0900

    マイグレーションファイルのエラーを対処

--- db/migrate/20200310011025_create_posts.rb
+++ db/migrate/20200310011025_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :text
-      t.text :image, null: false,
+      t.text :image, null: false
       t.timestamps
     end
   end
